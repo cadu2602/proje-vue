11322922dad5c889bcd0b03d94be9cc244a5fd8e
Partithura <partithura@pop-os.localdomain>
2022-04-04 08:47:10 -0300

    ✨ first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+  <Life />
+  </div>
+</template>
+
+
+<script>
+import Life from "./components/lifeCycle.vue"
+
+export default {
+  name: "App",
+  components: {
+    Life
+  },
+};
+</script>
---
+++ src/components/lifeCycle.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>LifeCycle hooks</h1>
+</template>
+
+<script>
+import { onMounted } from 'vue'
+export default {
+  setup() {
+    onMounted(() => {
+      console.log('Composing API')
+    })
+  },
+};
+
+// export default {
+//   name: "LifeCycle",
+//   mounted() {
+//     console.log('i dont now')
+//   },
+// };
+</script>
+
+<style>
+</style>
